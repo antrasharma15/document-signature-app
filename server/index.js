909039680bf114aec29d32b7cca2468384e8efa1
antrasharma15 <antra150906@gmail.com>
2026-06-07 19:55:39 +0530

    Day 1: project setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => res.send('Server is running ✅'));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected ✅');
+    app.listen(5000, () => console.log('Server running on port 5000'));
+  })
+  .catch(err => console.log(err));
